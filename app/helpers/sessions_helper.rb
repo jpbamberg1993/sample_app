6212ba3e9a95169dd335feaac6da805f9f20444d
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-09-25 23:37:19 -0400

    user login throws errors

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,17 @@
+module SessionsHelper
+
+	# Logs in the given user.
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	# Return the current logged-in user (if any).
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# Return true if user logged in, and false otherwise.
+	def logged_in?
+		:current_user.nil?
+	end
+end
