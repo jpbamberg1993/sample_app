2c73e308f42f919ad2cf77424b31a9e26c96ffaa
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-10-29 20:10:48 -0400

    Test links and admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -48,7 +49,10 @@ class UsersController < ApplicationController
 private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		params.require(:user).permit( :name,
+                                  :email,
+                                  :password,
+                                  :password_confirmation )
 	end
 
   # Before filters
