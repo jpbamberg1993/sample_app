e710ac57d682c908e1b9b9dc0995462dc72e9bc9
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-10-29 23:05:03 -0400

    new method for downcasing email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
 	attr_accessor :remember_token, :activation_token
-	before_save :downcase_email # before_save { email.downcase! } 
+	has_secure_password
+	before_save :downcase_email
 	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\s\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
 					  format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
-	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	class << self
@@ -41,16 +41,16 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
-	private
+private
 
-	# Converts email to all lower-case.
-	def dowcase_email
-		self.email = email.downcase
-	end
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
 
-	# Creates and assigns the activationtoken and digest.
-	def create_activation_digest
-		self.activation_token = User.new_token
-		self.activation_digest = User.digest(activation_token)
-	end
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
