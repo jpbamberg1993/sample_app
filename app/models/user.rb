4453c3dde30755fc128f8cb9d13c13a2b01333da
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-10-29 22:45:20 -0400

    set activation token before_create on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save { email.downcase! }
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email # before_save { email.downcase! } 
+	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\s\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
@@ -38,5 +39,18 @@ class User < ActiveRecord::Base
 	# Forgets the user
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	private
+
+	# Converts email to all lower-case.
+	def dowcase_email
+		self.email = email.downcase
+	end
+
+	# Creates and assigns the activationtoken and digest.
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
 	end
 end
